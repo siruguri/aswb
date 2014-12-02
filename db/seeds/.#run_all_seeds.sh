f709016bfd9b1266cd51e7f5da70aac9ea045233
Sameer Siruguri <siruguri@gmail.com>
2014-12-02 11:13:54 -0800

    unnecessary file in prev commit

--- db/seeds/.#run_all_seeds.sh
+++
@@ -1,1 +0,0 @@
-sameer@localhost.43471
--- db/seeds/run_all_seeds.sh
+++ db/seeds/run_all_seeds.sh
@@ -1,5 +1,5 @@
 rake db:seed:make_users
+rake db:seed:make_navbar_entries
 rake db:seed:make_jurisdictions
 rake db:seed:make_licensed_practices
-rake db:seed:make_navbar_entries
 
